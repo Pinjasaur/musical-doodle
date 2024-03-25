e75ebc4d7c2c5a9a13d41d79a0edbc3d49cd9179
Paul Esch-Laurent <p@uly.io>
2024-03-24 21:21:12 -0500

    feat: implement song.link widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link, useParams } from "react-router-dom";
 import Axios from "axios";
 
 export default function App() {
+  const example = "Oasis Wonderwall (Remastered)"
+
   return (
     <div>
       <h1>Last.fm & Song.link Integration</h1>
@@ -13,8 +15,8 @@ export default function App() {
       </p>
 
       <p>
-        If you happen to know the <Link to="https://musicbrainz.org/doc/MusicBrainz_Identifier">MusicBrainz ID</Link>{' '}
-        you can use that in the URL directly e.g. <code>/musicbrainz-id-goes-here</code>.
+        You can also directly put the track (and artist) name in the URL, delimited by pluses, like so:{' '}
+        <Link to={example.split(" ").join("+")}>/{example.split(" ").join("+")}</Link>
       </p>
 
       {/* Routes nest inside one another. Nested route paths build upon
@@ -69,10 +71,11 @@ function Index() {
 
   return (
     <div>
-      <h2>Index</h2>
+      <h2>Top Tracks For The Last Month</h2>
       <ol>
       {tracks.map((track: any) => {
-        return <li>
+        // _sigh_, not all results have a MusicBrainz ID...
+        return <li key={track.mbid || (track.name + " by " + track.artist.name)}>
           <Link to={[track.artist.name, track.name].join(" ").split(" ").join("+")}>{track.name} by {track.artist.name}</Link> (played {track.playcount} time{track.playcount > 1 && 's'})
           </li>
       })}
@@ -82,9 +85,38 @@ function Index() {
 }
 
 function Detail() {
+  const { mbid } = useParams()
+  const [track, setTrack] = useState(undefined as any)
+  useEffect(() => {
+    ;(async () => {
+      const { data: res } = await Axios.get(`https://itunes.apple.com/search?term=${encodeURIComponent(mbid!.split("+").join(" "))}&country=US&entity=song`)
+      setTrack(res.results[0])
+    })()
+  }, [mbid])
+  const isDarkMode = () => window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+
   return (
     <div>
-      <h2>Detail</h2>
+      {mbid !== undefined && track !== undefined && <>
+      <h2>{track.trackName} by {track.artistName}</h2>
+
+      <p>Here&rsquo;s what the Song.link widget looks like:</p>
+
+      <iframe width="100%" height="150" src={`https://embed.odesli.co/?url=https%3A%2F%2Fsong.link%2Fi%2F${track.trackId}&theme=${isDarkMode() ? 'dark' : 'light'}`} allowFullScreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>
+
+      <hr />
+
+      <p>And here&rsquo;s the code to implement it:</p>
+
+      <pre>
+        <code>
+          {`<iframe width="100%" height="150" src="https://embed.odesli.co/?url=https%3A%2F%2Fsong.link%2Fi%2F${track.trackId}&theme=${isDarkMode() ? 'dark' : 'light'}" allowfullscreen sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" allow="clipboard-read; clipboard-write"></iframe>`}
+        </code>
+      </pre>
+      </> || <>
+      <p>Hmmm&hellip;looks like that didn't work. Try again or <Link to="/">go home</Link>.</p>
+      </>}
     </div>
   );
 }
